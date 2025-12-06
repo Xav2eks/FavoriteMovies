7db0136a5463b3c058d1259755390328126c08df
Xavier Bob Vitualla <xavierbob48@gmail.com>
2025-12-06 21:30:10 +0800

    Add an addBtn and handleTitleInput(incomplete)

--- src/components/CreateInput.jsx
+++ src/components/CreateInput.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 function CreateInput() {
+  const [titleInput, setTitleInput] = useState("");
+
+  function handleTitleInput(event) {
+    const titleValue = event.target.value;
+
+    setTitleInput(titleValue);
+
+    console.log(titleValue);
+  }
+
   return (
     <div className="input-container">
       <div className="title-input-container">
         <p>Title:</p>
-        <input placeholder="ADD A MOVIE" />
+        <input
+          placeholder="ADD A MOVIE"
+          onChange={handleTitleInput}
+          value={titleInput}
+        />
       </div>
       <div className="comment-input-container">
         <p>Comment:</p>
         <textarea />
+      </div>
+      <div className="button-container">
+        <button>ADD</button>
       </div>
     </div>
   );
