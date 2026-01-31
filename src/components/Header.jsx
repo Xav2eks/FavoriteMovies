80f7bca899e82c6f31420a5ab038cc3e1ef9c362
Xavier Bob Vitualla <xavierbob48@gmail.com>
2026-01-31 18:03:25 +0800

    Add routing from home to app vice versa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <header>
-            <h1>FAVORITE MOVIE WEBSITE</h1>
-        </header>
-    );
+  return (
+    <header>
+      <Link to="/" className="header-container">
+        <h1>FAVORITE MOVIE WEBSITE</h1>
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
