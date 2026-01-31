1660b8276b4247b7af11a7dab84f50ac1411f613
Xavier Bob Vitualla <xavierbob48@gmail.com>
2026-01-31 16:46:34 +0800

    Add route from home to app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <Header />;
+  return (
+    <div className="home-container">
+      <Header />
+      <div className="home-content-container">
+        <p>
+          Welcome to your personal film archive. This is the place to gather,
+          celebrate, and rediscover every movie that has ever moved you. Stop
+          trying to remember that perfect film you saw years ago, or scrambling
+          for a recommendation. Here, you can build your own digital collection,
+          rate each experience, and craft the definitive list of your all-time
+          favorites. It’s more than a list—it’s the story of your journey
+          through cinema.
+        </p>
+        <Link to="/App" className="app-button">
+          <label>GO TO APP</label>
+        </Link>
+      </div>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default Home;
